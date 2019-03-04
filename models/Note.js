260734640623227a9781d084f73cd9f6a438fe57
min <mjnorthwesternwork1121@gmail.com>
2019-03-04 05:51:14 -0600

    Adding syntax

--- models/Note.js
+++ models/Note.js
@@ -3,5 +3,16 @@ const mongoose = require("mongoose");
 
 let Schema = mongoose.Schema;
 
-var NoteSchema
+let NoteSchema = new Schema({
+
+    title: String,
+
+    body: String
+    
+});
+
+var Note = mongoose.model("Article", ArticleSchema);
+
+//export model
+module.exports = Article;
 
