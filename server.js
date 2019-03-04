745053a5d02e2ef1e24cbaa98d7b14e3eb5e2ddd
min <mjnorthwesternwork1121@gmail.com>
2019-03-04 06:30:09 -0600

    Adding handlebars

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bodyParser = require("bodyparser")
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require ("path");
 
 
 
@@ -10,7 +12,10 @@ var cheerio = require("cheerio");
 // models that are required
 let db = require("./models");
 
-const PORT = 3000;
+// process.env Global variable is injected by the Node at runtime for application to use and it represents the state of environment your application is in when it starts.
+const PORT = process.env.PORT || 3000;
+
+var MONGODB_URI = PRO 
 
 const app = express();
 
