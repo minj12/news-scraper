0a7b257e19d752cc6973abe43f6654e5a5775bb8
min <mjnorthwesternwork1121@gmail.com>
2019-03-06 17:47:41 -0600

    updateing models

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+//Makeing
+let ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+      },
+      // `link` is required and of type String
+      link: {
+        type: String,
+        required: true
+      },
+      compress: {
+        type: String,
+        required: true
+        
+      },
+      saved: {
+        type: Boolean,
+        default: false
+      },
+      note: [{
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+      }]
+});
+
+// creates th model from the above schema, using  model method for mongoose
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
+
+
+
